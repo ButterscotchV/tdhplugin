a673d25b468656224c85bc10f95e693333ea4c34
Dankrushen <henryclose@gmail.com>
2019-06-02 03:26:58 -0400

    Update INetworkResponseListener.cs

--- TDHPlugin/Networking/NetworkMessages/INetworkResponseListener.cs
+++ TDHPlugin/Networking/NetworkMessages/INetworkResponseListener.cs
@@ -5,5 +5,6 @@ namespace TDHPlugin.Networking.NetworkMessages
 	public interface INetworkResponseListener
 	{
 		void OnNetworkResponse([NotNull] NetworkRequest request, [NotNull] NetworkResponse response);
+		void OnNetworkRequestTimeout([NotNull] NetworkRequest request);
 	}
 }
